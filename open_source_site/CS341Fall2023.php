f716dc234e58a5363da4e212a0fd3a2dca4a7b0a
Professor Reddig <reddign@etown.edu>
2023-08-23 22:53:37 -0400

    Student Page setup.

---
+++ open_source_site/CS341Fall2023.php
@@ -0,0 +1,50 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
+session_start();
+$title = "Jay Pantry Contributors";
+
+require_once "includes/header.php";
+?>
+
+<body>
+    <div class="home">
+        <!-- <a href="https://bluejaypantry.etowndb.com/src/homePage/index.php">
+            <img src="images\home-big.png" alt="home icon" height="75px">
+        </a> -->
+    </div>
+    <div class="team-section">
+        <h1>CS 341 - Software Engineering - Fall 2023</h1>
+        <div class="section" style="text-align:center">Our class included:<BR>
+        <span class="border"></span>
+      
+            <a href='janedoe.php'>Jane Doe</a><BR>
+            Anfal Al-Hussaini<br>
+            AJ Botticelli<br>
+            Nathan Brightup<br>
+            James Buck<br>
+            Mina Campos<br>
+            Phillip Goldberg<br>
+            Clayton Greer<br>  
+            Sam Huhn<br>
+            Michael Kennedy<br>
+            Ethan Lajeunesse<br>
+            Emma Maykut<br>
+            Stephanie Motz<br>
+            Isabel Pacheco Mattivi<br>
+            Melissa Patton<br>
+            Nolan Pettit<br>
+            Giovanni Raso<br>
+            Wesley Ryan<br>
+            Danielle Strausburger<br>
+            Tanner Wetzel<br>
+            Toma Yasuda<br>
+        </div>
+    
+</div>
+   
+</body>
+
+<?php
+    require_once "includes/sidebar.php";
+    ?>
