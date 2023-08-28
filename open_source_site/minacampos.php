3ded877eccdc512efadfbaec697b3ad7862959bc
Mina Campos <camposm@etown.edu>
2023-08-27 21:47:48 -0400

    Update minacampos.php

---
+++ open_source_site/minacampos.php
@@ -0,0 +1,30 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
+session_start();
+$title = "Mina Campos";
+
+require_once "includes/header.php";
+?>
+
+<body>
+    <div class="home">
+        <!-- <a href="https://bluejaypantry.etowndb.com/src/homePage/index.php">
+            <img src="images\home-big.png" alt="home icon" height="75px">
+        </a> -->
+    </div>
+    <div class="team-section">
+        <h1>Mina Campos</h1>
+        <div class="section">Mina Campos is an engineering student with a concentration in computers. She is expecting to graduate in 2026.
+       <BR><BR>
+        <a href="CS341Fall2023.php">Back to CS 341 - Fall 2023</a>   
+    </div>
+        
+    
+</div>
+   
+</body>
+
+<?php
+    require_once "includes/sidebar.php";
+    ?>
