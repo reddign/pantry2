e88e2d59ec568db1d7acb2581ed60917d74186c2
Ethan Lajeunesse <ethan@lajfam.com>
2023-08-27 23:08:25 -0400

    Improve FoodDatabase

--- data_src/classes/FoodDatabase.php
+++ data_src/classes/FoodDatabase.php
@@ -1,19 +1,27 @@
 <?PHP
 
 class FoodDatabase{
+    private static $conn = null;
+
+    private static function connect() {
+        global $servername, $database, $username, $password;
+
+        if (self::$conn === null) {
+            try {
+                self::$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+                http_response_code(500);
+                exit();
+            }
+        }
+    }
 
     public static function getDataFromSQL($sql,$params=null){
-        global $servername,$database,$username,$password;
-        try {
-        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected successfully";
-        } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-        }
+        self::connect();
         
-        $stmt = $conn->prepare($sql);
+        $stmt = self::$conn->prepare($sql);
         $stmt->execute($params);
         //$stmt->execute();
         
@@ -22,26 +30,26 @@ class FoodDatabase{
         $valuesArray = $stmt->fetchAll();
         return $valuesArray;
     }
+
     public static function executeSQL($sql,$params=null){
-        global $servername,$database,$username,$password;
-        try {
-        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected successfully";
-        } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-        }
+        self::connect();
         
-        $stmt = $conn->prepare($sql);
+        $stmt = self::$conn->prepare($sql);
         $stmt->execute($params);
-        //$stmt->execute();
-        
-        // set the resulting array to associative
         return true;
     }
 
+    public static function startTransaction() {
+        self::connect();
+        self::$conn->beginTransaction();
+    }
 
+    public static function commitTransaction() {
+        self::$conn->commit();
+    }
 
+    public static function rollbackTransaction() {
+        self::$conn->rollback();
+    }
 }
 ?>
