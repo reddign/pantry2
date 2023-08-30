9c2d745cc86fd2a940924937bc2de2023e9e8488
emmaykut <emmamaykut@gmail.com>
2023-08-30 14:28:00 -0400

    added link

--- open_source_site/CS341Fall2023.php
+++ open_source_site/CS341Fall2023.php
@@ -29,8 +29,7 @@ require_once "includes/header.php";
             <a href="SamHuhn.php">Sam Huhn</a><br>
             <a href= 'michaelkennedy.php'>Michael Kennedy</a><br>
             <a href='ethanlaj.php'>Ethan Lajeunesse</a><br>
-            Emma Maykut<br>
-            Professor Reddig <BR>
+            <a href = 'emmamaykut.php' > Emma Maykut </a><br>
             <a href='stephaniemotz.php'>Stephanie Motz</a><br>
             <a href='isabelpmattivi.php'>Isabel Pacheco Mattivi</a><br>
             <a href='MelissaPatton.php'>Melissa Patton</a><BR>
