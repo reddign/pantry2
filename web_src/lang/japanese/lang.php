bc6e779314cf38b02b9efe5169e7c004c0f1fd24
wetzelTanner <140431902+wetzelTanner@users.noreply.github.com>
2023-08-31 10:34:33 -0400

    Added variables to non-english language files

--- web_src/lang/japanese/lang.php
+++ web_src/lang/japanese/lang.php
@@ -1,2 +1,34 @@
 <?php
+//MAIN PAGE
+//inventory
+$add_button = "";
+$delete_button = "";
+//header buttons
+$cart_button = "";
+$about_button = "";
+$contact_button = "";
+$logon_button = "";
+$inventory_button = "";
+$donate_button = "";
+//donations
+$donate_here = ""; //Money
+$food_find_more = ""; //Food
+$time_sign_up = ""; //Time
+//contact section
+$contact = "";
+    //send message form
+    $name = "";
+    $email = "";
+    $subject = "";
+    $message = "";
+    $send_message_button = "";
+//front page
+$learn_more_button = "";
+$view_inventory_button = "";
+//bottom of page
+$to_top_button = "";
+$etown_students_link = "";
+//Admin Login
+$username_entry = "";
+$password_entry = "";
 ?>
