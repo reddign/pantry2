bd607e1f1ecbf588f682f8c2ddaff3f4ec044010
Greer <greerc@etown.edu>
2023-08-31 10:24:32 -0400

    adding basic key values for home page

--- web_src/lang/eng-us/lang.php
+++ web_src/lang/eng-us/lang.php
@@ -1,2 +1,34 @@
 <?php
+//MAIN PAGE
+//inventory
+$add_button = "Add";
+$delete_button = "Delete";
+//header buttons
+$cart_button = "MY CART";
+$about_button = "ABOUT";
+$contact_button = "CONTACT";
+$logon_button = "LOGON";
+$inventory_button = "INVENTORY";
+$donate_button = "DONATE";
+//donations
+$donate_here = "Donate Here"; //Money
+$food_find_more = "Find Out More"; //Food
+$time_sign_up = "Sign Up"; //Time
+//contact section
+$contact = "CONTACT";
+    //send message form
+    $name = "Name";
+    $email = "Email";
+    $subject = "Subject";
+    $message = "message";
+    $send_message_button = "SEND MESSAGE";
+//front page
+$learn_more_button = "Learn more";
+$view_inventory_button = "View Our Inventory";
+//bottom of page
+$to_top_button = "To the top";
+$etown_students_link = "Etown CS Students";
+//Admin Login
+$username_entry = "Type your username";
+$password_entry = "Type your password";
 ?>
