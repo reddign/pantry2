ab2faba88ce01e3f04637a89ad82141b44df83e2
wetzelTanner <140431902+wetzelTanner@users.noreply.github.com>
2023-09-14 10:12:28 -0400

    reformatted all existing French translations

--- web_src/lang/french/lang.php
+++ web_src/lang/french/lang.php
@@ -1,36 +1,100 @@
 <?php
-//MAIN PAGE
-//inventory
-$add_button = "Ajouter";
-$delete_button = "Supprimer";
-//header buttons
-$cart_button = "MON PANIER";
-$about_button = "À PROPOS";
-$contact_button = "CONTACT";
-$logon_button = "PLOGON";
-$inventory_button = "INVENTAIRE";
-$donate_button = "FAIRE UN DON";
-//donations
-$donate_here = "Faites un don ici"; //Money
-$food_find_more = "En savoir plus"; //Food
-$time_sign_up = "S'inscrire"; //Time
-//contact section
-$contact = "CONTACT";
-    //send message form
-    $name = "Nom";
-    $email = "E-mail";
-    $subject = "Sujet";
-    $message = "Message";
-    $send_message_button = "ENVOYER LE MESSAGE";
-//front page
-$welcome_message = "Nourriture gratuite pour les étudiants dans le besoin";
-$learn_more_button = "En savoir plus";
-$view_inventory_button = "Consultez notre inventaire";
-//bottom of page
-$to_top_button = "Jusqu'au sommet";
-$etown_students_link = "Étudiants d'Etown CS";
-//Admin Login
-$username_entry = "Tapez votre nom d'utilisateur";
-$password_entry = "Tapez votre mot de passe";
-$login_button = "SE CONNECTER";
+
+global $translations;
+
+$translations = array(
+    // Main Page
+    "add_button" => "Ajouter",
+    "delete_button" => "Supprimer",
+    "cart_button" => "MON PANIER",
+    "about_button" => "À PROPOS",
+    "contact_button" => "CONTACT",
+    "logon_button" => "PLOGON",
+    "inventory_button" => "INVENTAIRE",
+    "donate_button" => "FAIRE UN DON",
+    
+    // Donations
+    "donate_here" => "Faites un don ici", //Money
+    "food_find_more" => "En savoir plus", //Food
+    "time_sign_up" => "S'inscrire", //Time
+    
+    // Contact Section
+    "contact" => "CONTACT",
+    "contact_desc" => "",
+    "name" => "Nom",
+    "email" => "E-mail",
+    "subject" => "Sujet",
+    "message" => "Message",
+    "send_message_button" => "ENVOYER LE MESSAGE",
+    
+    // Front Page
+    "welcome_message" => "Nourriture gratuite pour les étudiants dans le besoin",
+    "learn_more_button" => "En savoir plus",
+    "view_inventory_button" => "Consultez notre inventaire",
+    
+    // Bottom of Page
+    "to_top_button" => "Jusqu'au sommet",
+    "powered_by" => "Propulsé par les",
+    "etown_students_link" => "Étudiants d'Etown CS",
+    
+    // Admin Login
+    "username_entry" => "Tapez votre nom d'utilisateur",
+    "password_entry" => "Tapez votre mot de passe",
+    "login_button" => "SE CONNECTER",
+    
+    // Additional Text Translations
+    "Front_page_title" => "",
+    
+    // About Section
+    "About_header" => "",
+    "About_sub_header" => "Nourriture gratuite pour les étudiants dans le besoin",
+    "About_sub_paragraph" => "",
+    "About_sub_paragraph_2" => "",
+    
+    // Our Goals Section
+    "Our_goals_header" => "",
+    "Responsive" => "",
+    "Responsive_desc" => "",
+    "Accessible" => "",
+    "Accessible_desc" => "",
+    "Healthy_meals" => "",
+    "Healthy_meals_desc" => "",
+    
+    // Statistics
+    "about_statistics_students" => "",
+    "about_statistics_items_distributed" => "",
+    "about_statistics_meals_delivered" => "",
+    "about_statistics_families_fed" => "",
+    
+    // Accessibility
+    "we_are_accessible" => "",
+    "pantry_hours" => "",
+    "pantry_open" => "",
+    "location" => "",
+    "BSC251" => "Brossman Commons (BSC) 251",
+    "contact_info" => "",
+    "Center_for_civic" => "Center for Community and Civic Engagement |",
+
+    // Donation section
+    "Donation_Header" => "",
+    "Donation_desc" => "",
+    
+    "Food_head" => "",
+    "Food_desc1" => "",
+    "Food_desc2" => "",
+    "Food_desc3" => "",
+    "Food_desc4" => "",
+    "Food_desc5" => "",
+
+    "Money_Head" => "",
+    "Money_desc1" => "",
+    "Money_desc2" => "PayPal/Venmo",
+    "Money_desc3" => "",
+
+    "Time_head" => "",
+    "Time_desc1" => "",
+    "Time_desc2" => "",
+    "Time_desc3" => "",
+    "Time_desc4" => ""
+);
 ?>
