dfa00fd86efb2e6d48e7731b51bd4738427c5ed0
Greer <greerc@etown.edu>
2023-09-14 09:56:25 -0400

    Adding new translations for japanese

--- web_src/lang/japanese/lang.php
+++ web_src/lang/japanese/lang.php
@@ -1,34 +1,70 @@
 <?php
+global $translations;
+
+$translations = array(
 //MAIN PAGE
+
 //inventory
-$add_button = "カートに入れる";
-$delete_button = "削除";
+"add_button" => "カートに入れる",
+"delete_button" => "削除",
+
 //header buttons
-$cart_button = "カート";
-$about_button = "アバウト";
-$contact_button = "連絡";
-$logon_button = "ログイン";
-$inventory_button = "在庫";
-$donate_button = "寄付する";
+"cart_button" => "カート",
+"about_button" => "アバウト",
+"contact_button" => "連絡",
+"logon_button" => "ログイン",
+"inventory_button" => "在庫",
+"donate_button" => "寄付する",
+
 //donations
-$donate_here = "寄付する"; //Money
-$food_find_more = "調べる"; //Food
-$time_sign_up = "サインアップ"; //Time
+"donate_here" => "寄付する", //Money
+"food_find_more" => "調べる", //Food
+"time_sign_up" => "サインアップ", //Time
+
 //contact section
-$contact = "連絡";
-    //send message form
-    $name = "名前";
-    $email = "メール";
-    $subject = "内容";
-    $message = "メッセージ";
-    $send_message_button = "メッセージを送る";
+"contact" => "連絡",
+"contact_desc" => "連絡しましょう. メッセージを送ります:", //new
+//send message form
+"name" => "名前",
+"email" => "メール",
+"subject" => "内容",
+"message" => "メッセージ",
+"send_message_button" => "メッセージを送る",
+
 //front page
-$learn_more_button = "知る";
-$view_inventory_button = "在庫を見る";
+"learn_more_button" => "知る",
+"view_inventory_button" => "在庫を見る",
+
 //bottom of page
-$to_top_button = "トップへ戻る";
-$etown_students_link = "イータウンCSの大学生";
+"to_top_button" => "トップへ戻る",
+"powered_by" => "プロデューサーは", // new
+"etown_students_link" => "イータウンCSの大学生",
+
 //Admin Login
-$username_entry = "ユーザー名を入力";
-$password_entry = "パスワードを入力";
+"username_entry" => "ユーザー名を入力",
+"password_entry" => "パスワードを入力",
+
+// Donation section new
+"Donation_Header" => "寄付する",
+"Donation_desc" => "各種寄付を受け付けております。",
+
+"Food_head" => "食品",
+"Food_desc1" => "セリアル/パン",
+"Food_desc2" => "乳製品",
+"Food_desc3" => "新鮮な農産物",
+"Food_desc4" => "軽食",
+"Food_desc5" => "ヘルスケア用品",
+
+"Money_Head" => "金銭",
+"Money_desc1" => "現金/チェック",
+"Money_desc2" => "ペイパル/ベンモ",
+"Money_desc3" => "自動寄付する",
+
+"Time_head" => "チーメ",
+"Time_desc1" => "品出しを仕込む",
+"Time_desc2" => "ゲストを助ける",
+"Time_desc3" => "寄付を処理する",
+"Time_desc4" => "在庫を管理する"
+
+);
 ?>
