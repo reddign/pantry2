9975978696b6c9a940cb10d310d891288c1cc66a
Stephaniemotz <smotz0328@gmail.com>
2023-09-17 17:40:55 -0400

    Adding TODO's and comments

--- open_source_site/about.php
+++ open_source_site/about.php
@@ -2,13 +2,22 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL & ~E_NOTICE);
 session_start();
-$title = "About BlueJay Pantry";
+$title = "About BlueJay Pantry"; 
+
+//TODO: Paramertize paragraphs, no Hardcoding!! 
+
+//TODO: Define variables here for paragraph paramertization
+//$paragraph1, $paragraph2, etc. 
+
+//TODO: Define varaibles here for titles of paragraphs 
+// $title1, $title2, etc. 
+
 
 require_once "includes/header.php";
 
 ?>
 
-
+ 
 <body id = "AboutBody">
     </div>
     <!-- <div class="bannerClass">
@@ -16,28 +25,38 @@ require_once "includes/header.php";
     </div> -->
     <!-- <span class="border"></span> -->
     <div class="paragraph1">
-        <h1>Blue Jay Pantry</h1>
-        <p>
+
+    <!-- Free Food for Students in Need Paragraph, hardcoded -->
+        <h1>Blue Jay Pantry</h1> 
+        <p> 
             Elizabethtown College is committed to ensuring that all students have regular access to healthy food options. 
-            The Blue Jay Pantry has been established by our Center for Community and Civic Engagement to provide our students with free, non-perishable food items sourced by charitable donations from within our community.
+            The Blue Jay Pantry has been established by our Center for Community and Civic Engagement to provide our students with free, 
+            non-perishable food items sourced by charitable donations from within our community. 
             <br>
             <br>
             Our College joins over 600 schools nationwide who operate an on-campus food pantry and is a member of the College and University Food Bank Alliance (CUFBA).
-             CUFBA provides colleges and universities with support, training and resources to connect more students with the food and resources they need for educational success.
+             CUFBA provides colleges and universities with support, training and resources to connect more students with the food and resources they need for educational success. <!-- hardcode -->
         </p>
     </div>
     <span class="border"></span>
     <div class="paragraph2">
-        <h1>About The Program</h1>
+        <h1>About The Program</h1> <!-- TODO: Find location of line on website -->
         <h2>Our Goals:</h2>
         <ul>
-            <li>The pantry exists to help eliminate food insecurity at Elizabethtown College.</li>
-            <li>The pantry is intended to be accessible to all students in order to eliminate barriers to access for students experiencing hunger and having difficulty buying food and will operate in ways that maximize hospitality and privacy.</li>
-            <li>The pantry, in partnership with offices and programs on campus, will provide resources that will help students create healthy meals.</li>
+            <li>The pantry exists to help eliminate food insecurity at Elizabethtown College.</li> <!-- "Responsible" Paragrpah, hardcoded --> 
+            <li>The pantry is intended to be accessible to all students in order to eliminate barriers 
+                to access for students experiencing hunger and having difficulty buying food and will 
+                operate in ways that maximize hospitality and privacy.</li> <!-- "Accesible" Paragraph, hardcoded -->
+            <li>The pantry, in partnership with offices and programs on campus, will provide resources that 
+                will help students create healthy meals.</li> <!-- "Healthy Meals" Paragraph, hardcoded -->
         </ul>
         <br>
         <span class="border"></span>
-        <!-- <br> -->
+
+
+
+        <!-- TODO: After this point in the code, I can't find the correlating text on the site. Find where this goes to.-->
+
         <h2>How it Works:</h2>
         <p>The Blue Jay Pantry is accessible to all undergraduate and graduate Elizabethtown College students. 
             Should you require items while the College is closed, contact <a href="mailto:thompsonjavita@etown.edu">Javita Thompson</a>.</p>
