45984a2ab5e3940ed63164ec2cd980f46c2cc7db
MelissaPatton <pattonm1@etown.edu>
2023-09-19 11:11:47 -0400

    conflict

--- open_source_site/CS341Fall2023.php
+++ open_source_site/CS341Fall2023.php
@@ -25,8 +25,8 @@ require_once "includes/header.php";
         From Left to Right:
         <a href = 'minacampos.php'>Mina Campos</a>,  
         <a href= 'michaelkennedy.php'>Michael Kennedy</a>,
-        <a href='MelissaPatton.php'>Melissa GRACEEEEEE Pattttttttkkktttttgggtttttton</a>,
-        <a href="phillipgoldberg.php">Philllllllllllllllkkkkkkkkklllllgggglllllll</a>,
+        <a href='MelissaPatton.php'>Melissa GRACEEEEEE Paaaattttttttkkktttttgggtttttton</a>,
+        <a href="phillipgoldberg.php">Phiiiilllllllllllllllkkkkkkkkklllllgggglllllll</a>,
             <a href= 'anfal_alhussaini.php'>Anfal Al-Hussaini</a>
             <a href = 'ajbotticelli.php'>AJ Botticelli</a>,
             <a href='tanner_wetzel.php'>Tanner Wetzel</a>,
