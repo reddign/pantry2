f5e647e85119ceaf9cf9ec421ed9e121cf4f21f9
MelissaPatton <pattonm1@etown.edu>
2023-09-19 10:49:28 -0400

    GIT conflict

--- data_src/api/reports/read.php
+++ data_src/api/reports/read.php
@@ -34,7 +34,7 @@ if($graphType=="ByProduct"){
 //TODO make this sql work
 else if($graphType=="ByUserInfo"){
   $sql = "select userID, SUM(children), SUM(adult), SUM(senior), SUM(firstuse)
-  from registration";
+  from registration GROUP BY userID";
   $params = null;
 }
 
