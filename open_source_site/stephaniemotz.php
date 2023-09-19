18dabf6c85bb2f99512f51425610845f4738e0cc
Stephaniemotz <smotz0328@gmail.com>
2023-09-19 09:32:24 -0400

    Changing my page back after conflict

--- open_source_site/stephaniemotz.php
+++ open_source_site/stephaniemotz.php
@@ -16,7 +16,6 @@ require_once "includes/header.php";
     <div class="team-section">
         <h1>Stephanie Motz</h1>
         <div class="section">Stephanie Motz is a junior Computer Science major with a Graphic Design minor. She has a concentration in Web and Application Design, and plans to graduate in 2025.
-        <div class="section">Steph Motz is a junior at Elizabethtown College. She is a Computer Science major with a Graphic Design minor. She has a concentration in Web and Application Design, and plans to graduate in 2025.
        <BR><BR>
         <a href="CS341Fall2023.php">Back to CS 341 - Fall 2023</a>   
     </div>
