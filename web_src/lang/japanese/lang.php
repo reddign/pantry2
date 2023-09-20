a2f0230f3f26b5bde5f1937b6f86f3f6cc5d294e
BuckJ12 <cyanblanket@gmail.com>
2023-09-20 15:12:05 -0400

    Extra Vars needed in Japanese

--- web_src/lang/japanese/lang.php
+++ web_src/lang/japanese/lang.php
@@ -46,6 +46,40 @@ $translations = array(
 "username_entry" => "ユーザー名を入力",
 "password_entry" => "パスワードを入力",
 
+// Additional Text Translations
+"Front_page_title" => "Blue Jay Pantry",
+    
+// About Section
+"About_header" => "ABOUT",
+"About_sub_header" => "Free Food for Students In Need",
+"About_sub_paragraph" => "Elizabethtown College is committed to ensuring that all students have regular access to healthy food options. The Blue Jay Pantry has been established by our Center for Community and Civic Engagement to provide our students with free, non-perishable food items sourced by charitable donations from within our community...",
+//new variable
+"About_sub_paragraph_2" => "Our College joins over 600 schools nationwide who operate an on-campus food pantry and is a member of the College and University Food Bank Alliance (CUFBA). CUFBA provides colleges and universities with support, training and resources to connect more students with the food and resources they need for educational success.",
+
+// Our Goals Section
+"Our_goals_header" => "OUR GOALS",
+"Responsive" => "Responsive",
+"Responsive_desc" => "The pantry exists to help eliminate food insecurity at Elizabethtown College...",
+"Accessible" => "Accessible",
+"Accessible_desc" => "The pantry is intended to be accessible to all students in order to eliminate barriers to access for students experiencing hunger...",
+"Healthy_meals" => "Healthy Meals",
+"Healthy_meals_desc" => "The pantry, in partnership with offices and programs on campus, will provide resources that will help students create healthy meals...",
+
+// Statistics
+"about_statistics_students" => "Students Helped",
+"about_statistics_items_distributed" => "Items Distributed",
+"about_statistics_meals_delivered" => "Meals Delivered",
+"about_statistics_families_fed" => "Families Fed",
+
+// Accessibility
+"we_are_accessible" => "We are accessible",
+"pantry_hours" => "Pantry Hours:",
+"pantry_open" => "Pantry is open 24/7",
+"location" => "Location",
+"BSC251" => "Brossman Commons (BSC) 251",
+"contact_info" => "Contact Information:",
+"Center_for_civic" => "Center for Community and Civic Engagement |",
+
 // Donation section new
 "Donation_Header" => "寄付する",
 "Donation_desc" => "各種寄付を受け付けております。",
