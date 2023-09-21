2d51c5f2b06956cfa50ac3ee3a335c2a8fc27d07
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-09-21 09:41:24 -0400

    update bar graph function by category

--- web_src/classes/GoogleChartDisplay.php
+++ web_src/classes/GoogleChartDisplay.php
@@ -3,34 +3,37 @@ class GoogleChartDisplay{
 
     
     public static function getByCategoryReport($data){
-        $content = GoogleChartDisplay::getGoogleJSForBarGraph($data,"Products Grabbed Within A Category","Total Amount For Each Product","Product Types");
+        $type="category";
+        $content = GoogleChartDisplay::getGoogleJSForBarGraph($data,"Products Grabbed Within A Category","Total Amount For Each Product","Product Types", $type);
         return $content;
     }
     public static function getTotalReport($data){
-
-        $content = GoogleChartDisplay::getGoogleJSForBarGraph($data,"Total Food Grabbed","Total Amount","CategoryID"); 
+        $type="total";
+        $content = GoogleChartDisplay::getGoogleJSForBarGraph($data,"Total Food Grabbed","Total Amount","CategoryID", $type); 
         return $content;
         
     }
     public static function getByDateReport($data){
-        $content = GoogleChartDisplay::getGoogleJSForBarGraph($data,"Orders on specific dates","Total Products","Date"); 
+        $type="date";
+        $content = GoogleChartDisplay::getGoogleJSForBarGraph($data,"Orders on specific dates","Total Products","Date", $type); 
        
           return $content;
     }
 
     public static function getUserData($data) {
-        $content = GoogleChartDisplay::getGoogleJSForBarGraph($data, "User Info", "User Types", "Total Users");
+        $type="userInfo";
+        $content = GoogleChartDisplay::getGoogleJSForBarGraph($data, "User Info", "User Types", "Total Users", $type);
         return $content;
     }
-
-    private static function getGoogleJSForBarGraph($data,$title,$xscale,$yscale){
+    
+    private static function getGoogleJSForBarGraph($data,$title,$xscale,$yscale, $type){
         $content ='
         <script type="text/javascript">
         google.load("visualization", "1", {packages:["corechart"]});
         google.setOnLoadCallback(drawChart); 
         function drawChart() {
         var data = google.visualization.arrayToDataTable([';
-        $content .= GoogleChartDisplay::getJSArrayData($data,$xscale,$yscale);
+        $content .= GoogleChartDisplay::getJSArrayData($data,$xscale,$yscale, $type);
         $content .= ']);';
 
         $content .= 'var chart = new google.visualization.BarChart(document.getElementById("Bar"));
@@ -42,19 +45,25 @@ class GoogleChartDisplay{
           $content .= GoogleChartDisplay::getDiv();
           return $content;
     }
-    private static function getJSArrayData($data, $titlex, $titley) {
+    private static function getJSArrayData($data,$titlex,$titley, $type){
         $array_string = '["'.$titlex.'","'.$titley.'"],';
-        foreach($data as $info) {
-            $userID = isset($info->userID) ? $info->userID : '';
-            $children = isset($info->{'SUM(children)'}) ? $info->{'SUM(children)'} : 0;
-            $adult = isset($info->{'SUM(adult)'}) ? $info->{'SUM(adult)'} : 0;
-            $senior = isset($info->{'SUM(senior)'}) ? $info->{'SUM(senior)'} : 0;
-            $firstuse = isset($info->{'SUM(firstuse)'}) ? $info->{'SUM(firstuse)'} : 0;
-    
-            $array_string .= "['Children',".$children."],";
-            $array_string .= "['Adult',".$adult."],";
-            $array_string .= "['Senior',".$senior."],";
-            $array_string .= "['First Use',".$firstuse."],";
+        if($type==="userInfo"){
+            foreach($data as $info) {
+                $userID = isset($info->userID) ? $info->userID : '';
+                $children = isset($info->{'SUM(children)'}) ? $info->{'SUM(children)'} : 0;
+                $adult = isset($info->{'SUM(adult)'}) ? $info->{'SUM(adult)'} : 0;
+                $senior = isset($info->{'SUM(senior)'}) ? $info->{'SUM(senior)'} : 0;
+                $firstuse = isset($info->{'SUM(firstuse)'}) ? $info->{'SUM(firstuse)'} : 0;
+        
+                $array_string .= "['Children',".$children."],";
+                $array_string .= "['Adult',".$adult."],";
+                $array_string .= "['Senior',".$senior."],";
+                $array_string .= "['First Use',".$firstuse."],";
+            }
+        }else{
+            foreach($data as $info){
+                $array_string .= "['".$info->productName."',".$info->total."],";
+            }
         }
         return $array_string;
     }
