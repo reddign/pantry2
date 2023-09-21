9f44a8522ead6fcaa29d1a40766c1a3cab84b82f
danistrausburger <dannysue2020@gmail.com>
2023-09-21 10:33:05 -0400

    testing changes

---
+++ open_source_site/css/stylecolors.php
@@ -0,0 +1,10 @@
+<?php
+
+$dark_bg = '#0A2240';
+$med_bg = '#004B98';
+$light_bg = '#c8c8c8';
+$med_border = '3DB5E6';
+$dark_border = '#000000';
+$error_color = 'FF0000';
+
+?>
---
+++ open_source_site/css/styles.php
@@ -0,0 +1,44 @@
+
+:root {
+  --med_bg: $data->med_bg_color;
+  --light_bg: <?php echo $light_bg; ?>;
+}
+
+ <!-- .content{ 
+    /* margin-left: 15%;
+    margin-top: 22%; 
+ -->
+.table {
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    margin: auto;
+    /* padding-left: 230px; */
+  }
+
+  .table tr {
+    width: 17%;
+  }
+
+  .table tr td {
+    border: solid black;
+    border-width: 5px;
+    background-color: var(--light_bg);
+  }
+
+  .productImg {
+    height: 300px;
+    width:225px;
+    padding: 7px;
+    background-color: var(--light_bg);
+
+  }
+
+  .addCart{
+    background-color: var(--med_bg);
+    color: var(--light_bg);
+    padding: 5px;
+    font-weight: bold;
+    border-radius: 4px;
+    border-color: var(--med_bg);
+  }
