c636cb1e4b3077cd3c3020701b6e76019586dd11
wetzelTanner <140431902+wetzelTanner@users.noreply.github.com>
2023-09-21 09:25:41 -0400

    Added links to translations for all buttons

--- web_src/includes/footer.php
+++ web_src/includes/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="w3-center w3-dark-blue w3-padding-64">
-  <a href="index.php?page=about#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <a href="index.php?page=about#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i><?php echo $translations["to_top_button"]?></a>
   <div class="w3-xlarge w3-section">
     <i class="fa fa-facebook-official w3-hover-opacity"></i>
     <i class="fa fa-instagram w3-hover-opacity"></i>
