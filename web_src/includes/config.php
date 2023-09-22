4ae96884162741322719f2e7b2d8fc310abbcf3a
danistrausburger <dannysue2020@gmail.com>
2023-09-22 13:02:50 -0400

    fixing quotes

--- web_src/includes/config.php
+++ web_src/includes/config.php
@@ -5,17 +5,17 @@ $url = "http://127.0.0.1/pantry2";
 $api_key = "848429r2g";
 
 // path to the logo
-$logo1 = “images/ETOWN_Footer_Logo.png”;
+$logo1 = "images/ETOWN_Footer_Logo.png";
 
 //path to the second logo
-$logo2 = “web_src/images/BlueJayPantry-Logo-v4.png”;
+$logo2 = "web_src/images/BlueJayPantry-Logo-v4.png";
 
 
 //If you wanted to change this image
-$backgroundImage = “images/pantry.jpg”;
+$backgroundImage = "images/pantry.jpg";
 
-$pantryName = “Blue Jay Pantry”;
+$pantryName = "Blue Jay Pantry";
 
-$navBGColor = “w3-dark-blue”;
+$navBGColor = "w3-dark-blue";
 
  
